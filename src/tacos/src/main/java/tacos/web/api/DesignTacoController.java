ea2dad802e707cda10b0c0baed980b66ab331563
Ben Clark <beneclark@gmail.com>
2021-03-09 13:36:39 -0600

    Initial commit

---
+++ src/tacos/src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,36 @@
+package tacos.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+    private TacoRepository tacoRepo;
+
+    @Autowired
+    EntityLinks entityLinks;
+
+    public DesignTacoController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page).getContent();
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
+}
